6a15bf02079912790df83db237bc7379cfeaaa59
Vishal Prajapati <kumar672399@gmail.com>
2026-06-16 20:31:35 +0530

    first commit

--- server.js
+++ server.js
@@ -29,4 +29,10 @@ app.use("/api/users", userRoutes);
 // Error Handler
 app.use(errorHandler);
 
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
