e98d5eb8210253054725b346cb6dacead2eda649
Vishal Prajapati <kumar672399@gmail.com>
2026-07-11 21:08:53 +0530

    end point add

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { addQuestion, getQuestions } = require("../controllers/questionController");
 
 // POST  — question save karo (appid + version middleware already server.js mein hai)
-router.post("/get_question", addQuestion);
+router.post("/creat_question", addQuestion);
 
 // GET — sabhi questions fetch karo
-router.get("/", getQuestions);
+router.get("/get_question", getQuestions);
 
 module.exports = router;
